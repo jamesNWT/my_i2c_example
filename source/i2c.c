b1c6845c53fc3bbf4ff415612f07071d18e37e5c
James Wilcox <jameswilcox.98@gmail.com>
2026-08-28 23:29:25 -0300

    minimum viable toolchain done
    can build with cmake, flash with picotool, and monitor continously with tio

--- source/i2c.c
+++ source/i2c.c
@@ -1,8 +1,12 @@
 #include <pico/stdio.h>
+#include <pico/stdio_usb.h>
+#include <pico/time.h>
 #include <stdio.h>
 
 int main() {
   stdio_init_all();
+  while (!stdio_usb_connected()) {
+    sleep_ms(10);
+  }
   printf("Hello from my i2c example code!\n");
-  return 0;
 }
